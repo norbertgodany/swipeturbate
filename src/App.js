17bece12389ffef8e05de977b34d5862d7a2a9d7
Norbert Godany <norbert.godany@gmail.com>
2019-05-23 22:13:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest'
+  }
+}
+
+const url = "https://pt.protoawe.com/api/video-promotion/v1/client/list?psid=zkygge&pstool=421_1&campaign_id=110795&accessKey=8345cd885505b6e7bcfb634eb402ff86&ms_notrack=1&campaign=110795&type=&sexualOrientation=straight&forcedPerformers=&limit=25&primaryColor=%238AC437&labelColor=%23212121"
+
+const fetchVideos = async () => {
+  try {
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchVideos()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Switeturbate</h1>
     </div>
   );
 }
