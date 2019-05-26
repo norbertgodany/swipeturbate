b838161e391084c77f52810398b85d7118b06ef7
Norbert Godany <norbert.godany@gmail.com>
2019-05-26 14:00:50 +0200

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/style.css';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest'
+class App extends Component {
+
+  fetchVideos = async () => {
+    const url = "https://pt.protoawe.com/api/video-promotion/v1/client/list?psid=zkygge&pstool=421_1&campaign_id=110795&accessKey=8345cd885505b6e7bcfb634eb402ff86&ms_notrack=1&campaign=110795&type=&sexualOrientation=straight&forcedPerformers=&limit=25&primaryColor=%238AC437&labelColor=%23212121";
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const { data } = await response.json();
+      const videos = data.videos;
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
 
-const url = "https://pt.protoawe.com/api/video-promotion/v1/client/list?psid=zkygge&pstool=421_1&campaign_id=110795&accessKey=8345cd885505b6e7bcfb634eb402ff86&ms_notrack=1&campaign=110795&type=&sexualOrientation=straight&forcedPerformers=&limit=25&primaryColor=%238AC437&labelColor=%23212121"
-
-const fetchVideos = async () => {
-  try {
-    const response = await fetch(url, options)
-    const data = await response.json()
-    console.log(data)
-    return data;
-  } catch (error) {
-    console.log(error)
+  componentDidMount() {
+    this.fetchVideos()
   }
-}
 
-fetchVideos()
-
-function App() {
-  return (
-    <div>
-      <h1>Switeturbate</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        {console.log(this.props.videos)}
+      </div>
+    )
+  }
 }
 
 export default App;
